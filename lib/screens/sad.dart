fbfb5d9369542096f19c7089571c286dee29478a
unknown <ynarula678@gmail.com>
2021-05-22 01:27:41 +0530

    files

---
+++ lib/screens/sad.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class SadPage extends StatefulWidget {
+  @override
+  _SadPageState createState() => _SadPageState();
+}
+
+class _SadPageState extends State<SadPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
