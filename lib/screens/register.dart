553de2bbabdf37731b5e57bce79657dd425e73b2
unknown <ynarula678@gmail.com>
2021-05-22 01:12:52 +0530

    home.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Text('Register'),
+          ],
+        ),
+      ),
+    );
+  }
+}
