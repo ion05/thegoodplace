ec17485c0c270754991a9ec5c1b6d03c4b9770d2
Yuvraaj Narula <49155095+yuvraajnarula@users.noreply.github.com>
2021-05-22 23:02:16 +0530

    fixes

--- lib/screens/angry.dart
+++ lib/screens/angry.dart
@@ -11,7 +11,7 @@ class AngryPage extends StatefulWidget {
 class _AngryPageState extends State<AngryPage> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
         body: Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
@@ -29,7 +29,7 @@ class _AngryPageState extends State<AngryPage> {
       child: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
           Text(
-            "Here's what you can do!",
+            "Here's something what you can do!",
             style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w700, color: Colors.white, fontFamily: "Nunito-Sans"),
           ),
           SizedBox(
@@ -39,6 +39,8 @@ class _AngryPageState extends State<AngryPage> {
             decoration: InputDecoration(
                 //hintText('Why are you angry?'),
                 ),
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
           ),
           SizedBox(height: 50.0),
           Column(
@@ -111,52 +113,73 @@ class _AngryPageState extends State<AngryPage> {
           Column(
             children: [
               ElevatedButton(
-                  onPressed: () {
-                    launch("https://open.spotify.com/playlist/37i9dQZF1DX7yRWDZJQ3Yz?si=-xt9mYF6R36YzfbLmEkwvw&utm");
-                  },
-                  child: Row(
-                    children: [
-                      Image(
-                        height: 75.0,
-                        width: 75.0,
-                        image: AssetImage("assets/images/vibe.png"),
+                onPressed: () {
+                  launch("https://open.spotify.com/playlist/37i9dQZF1DX7yRWDZJQ3Yz?si=-xt9mYF6R36YzfbLmEkwvw&utm");
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
-                      Text('Vibe', style: TextStyle(fontSize: 18.0, fontFamily: 'OpenSans-Condensed')),
-                      SizedBox(
-                        height: 2.0,
-                        width: 10.0,
-                      ),
-                    ],
-                  ),
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(EdgeInsets.zero),
-                  )),
+                    )),
+                child:
+                Padding( 
+                  padding: EdgeInsets.all(5.0),
+                  Row(
+                  children: [
+                    Image(
+                      height: 55.0,
+                      width: 55.0,
+                      image: AssetImage("assets/images/vibe.png"),
+                    ),
+                    SizedBox(width: 7.5,),
+                    Text(
+                      'Vibe',
+                      style: TextStyle(fontSize: 20.0, fontFamily: 'OpenSans-Condensed', color: Colors.black, ),
+                    ),
+                    SizedBox(
+                      height: 2.0,
+                      width: 10.0,
+                    ),
+                  ],
+                ),
+                ),
+              ),
               SizedBox(
                 height: 2.0,
                 width: 10.0,
               ),
               SizedBox(height: 10.0),
               ElevatedButton(
-                  onPressed: () {
-                    launch("https://open.spotify.com/playlist/37i9dQZF1DX3rxVfibe1L0?si=JzsnHyQ8Qs69ApAMHt39KA&utm");
-                  },
-                  child: Row(
-                    children: [
-                      Image(
-                        height: 75.0,
-                        width: 75.0,
-                        image: AssetImage("assets/images/mood.png"),
+                onPressed: () {
+                  launch("https://open.spotify.com/playlist/37i9dQZF1DX3rxVfibe1L0?si=JzsnHyQ8Qs69ApAMHt39KA&utm");
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
-                      Text('Mood Booster', style: TextStyle(fontSize: 18.0, fontFamily: 'OpenSans-Condensed')),
-                      SizedBox(
-                        height: 2.0,
-                        width: 10.0,
+                    )),
+                child: Row(
+                  children: [
+                    Image(
+                      height: 55.0,
+                      width: 55.0,
+                      image: AssetImage("assets/images/mood.png"),
+                    ),
+                    SizedBox(
+                      width: 7.0,
                       ),
-                    ],
-                  ),
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(EdgeInsets.zero),
-                  ))
+                    Text('Mood Booster', style: TextStyle(fontSize: 20.0, fontFamily: 'OpenSans-Condensed', color: Colors.black,)),
+                    SizedBox(
+                      height: 2.0,
+                      width: 10.0,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ]),
